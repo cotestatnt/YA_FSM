5689f3528a96abeb091a51598551f5d1660c96b9
Tolentino Cotesta <cotestatnt@yahoo.com>
2021-07-14 19:23:38 +0200

    Update YA_FSM.cpp

--- src/YA_FSM.cpp
+++ src/YA_FSM.cpp
@@ -1,14 +1,14 @@
 #include "YA_FSM.h"
 
 
-FSM_State*  YA_FSM::CurrentState(){	
+FSM_State*  YA_FSM::CurrentState(){
 	return _currentState;
 }
 
 
 
 FSM_State*  YA_FSM::GetStateAt(uint8_t index){
-	for(FSM_State* state = _firstState; state != nullptr; state = state->nextState) 
+	for(FSM_State* state = _firstState; state != nullptr; state = state->nextState)
 		if(state->index == index )
 		 	return state;
 	return nullptr;
@@ -37,23 +37,23 @@ uint8_t YA_FSM::AddState(const char* name, uint32_t maxTime, uint32_t minTime,
 	state->minTime = minTime;
 	state->index = _stateIndex;
 	return _stateIndex;
-}	
+}
 
 
 uint8_t YA_FSM::AddState(const char* name, uint32_t maxTime, action_cb onEntering, action_cb onState, action_cb onLeaving){
-	return AddState(name, maxTime, 0, onEntering, onState, onLeaving);			
+	return AddState(name, maxTime, 0, onEntering, onState, onLeaving);
 }
 
 
 uint8_t YA_FSM::AddTransition(uint8_t inputState, uint8_t outputState, condition_cb condition){
-	
+
 	FSM_Transition *transition = new FSM_Transition();
-	if (_firstTransition == nullptr) 
+	if (_firstTransition == nullptr)
 		_firstTransition = transition;
 	else
 		_lastTransition->nextTransition = transition;
 	_lastTransition = transition;
-	
+
 	transition->Condition = condition;
 	transition->InputState = inputState;
 	transition->OutputState = outputState;
@@ -62,14 +62,14 @@ uint8_t YA_FSM::AddTransition(uint8_t inputState, uint8_t outputState, condition
 }
 
 uint8_t YA_FSM::AddTransition(uint8_t inputState, uint8_t outputState, bool &condition){
-	
+
 	FSM_Transition *transition = new FSM_Transition();
-	if (_firstTransition == nullptr) 
+	if (_firstTransition == nullptr)
 		_firstTransition = transition;
 	else
 		_lastTransition->nextTransition = transition;
 	_lastTransition = transition;
-	
+
 	transition->ConditionVar = &condition;
 	transition->InputState = inputState;
 	transition->OutputState = outputState;
@@ -120,7 +120,6 @@ uint8_t YA_FSM::GetState() const{
 	return _currentState->index;
 }
 
-// Return true if timeout
 void YA_FSM::SetTimeout(uint8_t index, uint32_t preset) {
 	FSM_State* state = GetStateAt(index);
 	if(state != nullptr ){
@@ -132,43 +131,43 @@ void YA_FSM::SetTimeout(uint8_t index, uint32_t preset) {
 bool YA_FSM::GetTimeout(uint8_t index){
 	FSM_State* state = GetStateAt(index);
 	if(state != nullptr ){
-		return state->maxTime;		
+		return state->timeout;
 	}
 	return false;
 }
 
- 
-// Return current state entering time 
+
+// Return current state entering time
 uint32_t YA_FSM::GetEnteringTime(uint8_t index) {
 	FSM_State* state = GetStateAt(index);
 	if(state != nullptr ){
-		return state->enterTime;	
+		return state->enterTime;
 	}
-	return 0;	
+	return 0;
 }
 
 
 
 
 bool YA_FSM::Update(){
-	bool stateChanged = false;
 
 	for(FSM_Transition* actualtr = _firstTransition; actualtr != nullptr; actualtr = actualtr->nextTransition) {
 		if(actualtr->InputState == _currentState->index){
 
 			// Check if state is on timeout
 			if(_currentState->maxTime > 0){
-				if( millis() - _currentState->enterTime  > _currentState->maxTime)	{
+				uint32_t milss = millis() - _currentState->enterTime;
+				if( milss  > _currentState->maxTime)	{
 					_currentState->timeout = true;
 				}
 			}
 
 			bool _trigger = false;
-			if(actualtr->Condition == nullptr)			
+			if(actualtr->Condition == nullptr)
 				_trigger = *(actualtr->ConditionVar);
 			else
 				_trigger = actualtr->Condition();
-			
+
 			if (_trigger){
 				// Check if state is on at least from minTime
 				if(_currentState->minTime > 0){
@@ -180,32 +179,31 @@ bool YA_FSM::Update(){
 
 				// One of the transitions has triggered, set the new state
 				if(_currentState->OnLeaving != nullptr)
-					_currentState->OnLeaving();		
-								
+					_currentState->OnLeaving();
+
 				FSM_State* targetState = GetStateAt(actualtr->OutputState);
-				_currentState = targetState;		
-				
+				_currentState = targetState;
+
 				_currentState->enterTime = millis();
 				_currentState->timeout = false;
 
 				if(_currentState->OnEntering != nullptr)
-					_currentState->OnEntering();						
-				stateChanged = true;
+					_currentState->OnEntering();
+				return true;
 			}
 
-			
+
 		}
 
 		if(_currentState->OnState != nullptr)
 			_currentState->OnState();
-				
 	}
-	return stateChanged;
+	return false;
 }
 
 
 
-void YA_FSM::initVariables(){	
+void YA_FSM::initVariables(){
 	for(uint8_t i=0; i<_numStates; i++){
 		AddState("", 0, nullptr, nullptr, nullptr);
 	}
